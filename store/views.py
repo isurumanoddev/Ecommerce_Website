361bfd4dfa798f244726e5a1284dde5c12b807be
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-02-12 09:00:46 +0530

    add templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def store(request):
+    context = {}
+    return render(request, "Store.html", context)
+
+
+def cart(request):
+    context = {}
+    return render(request, "Cart.html", context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, "Checkout.html", context)
