f5cb81589f17d1cf422726b1034af6773c601db9
isurumanoddev <isurumanod99@gmail.com>
2023-02-21 18:32:24 +0530

    add view

--- store/urls.py
+++ store/urls.py
@@ -4,11 +4,9 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    # path('',views.store,name="store" ),
-    # path('cart/',views.cart,name="cart" ),
-    # path('checkout/',views.checkout,name="checkout" ),
-    # path('',views.index,name="checkout" ),
-    path('drinks/<int:drink_name>', views.drinks, name="drink_name"),
+    path('',views.store,name="store" ),
+    path('cart/',views.cart,name="cart" ),
+    path('checkout/',views.checkout,name="checkout" ),
 
 
 ]
--- store/views.py
+++ store/views.py
@@ -2,30 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
 
 
-# def store(request):
-#     context = {}
-#     return render(request, "Store.html", context)
-#
-#
-# def cart(request):
-#     context = {}
-#     return render(request, "Cart.html", context)
-#
-#
-# def checkout(request):
-#     context = {}
-#     return render(request, "Checkout.html", context)
+def store(request):
+    context = {}
+    return render(request, "Store.html", context)
 
 
-def drinks(request, drink_name):
-    drink = {
-        '1' : 'type of coffee',
-        '2' : 'type of hot beverage',
-        '3': 'type of refreshment'
-    }
-    # type = drink["drink_name"]
+def cart(request):
+    context = {}
+    return render(request, "Cart.html", context)
 
-    return HttpResponse(f"<h2>{drink_name}</h2> " )
+
+def checkout(request):
+    context = {}
+    return render(request, "Checkout.html", context)
+
+
+
