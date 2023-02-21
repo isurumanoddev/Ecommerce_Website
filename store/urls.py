abd99c6b38ad167c3cee75124ff833b732dc2736
isurumanoddev <isurumanod99@gmail.com>
2023-02-21 18:31:34 +0530

    map urls

--- store/urls.py
+++ store/urls.py
@@ -4,9 +4,11 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    path('',views.store,name="store" ),
-    path('cart/',views.cart,name="cart" ),
-    path('checkout/',views.checkout,name="checkout" ),
+    # path('',views.store,name="store" ),
+    # path('cart/',views.cart,name="cart" ),
+    # path('checkout/',views.checkout,name="checkout" ),
+    # path('',views.index,name="checkout" ),
+    path('drinks/<int:drink_name>', views.drinks, name="drink_name"),
 
 
 ]
