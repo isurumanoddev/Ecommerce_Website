1a88fab691a839a3fec0e0be74b3eb57ace78f98
isurumanoddev <isurumanod99@gmail.com>
2023-02-24 17:43:14 +0530

    add products

--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from store.models import Product
+
 
 def store(request):
-    context = {}
-    return render(request, "Checkout.html", context)
+    products = Product.objects.all()
+    context = {"products":products}
+    return render(request, "Store.html", context)
 
 
 def cart(request):
