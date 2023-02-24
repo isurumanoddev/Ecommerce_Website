b03c977a1c32fbb600c8ecd1af79cd0f4647ae16
isurumanoddev <isurumanod99@gmail.com>
2023-02-24 17:14:54 +0530

    complete models

--- store/models.py
+++ store/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
@@ -34,9 +32,23 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
-    quantity = models.IntegerField(default=0,null=True,blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zip_code = models.CharField(max_length=200, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+
