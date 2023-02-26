32c2466446b255b825599c96fe93670e359154da
isurumanoddev <isurumanod99@gmail.com>
2023-02-26 09:56:00 +0530

    add product image to database

--- store/models.py
+++ store/models.py
@@ -15,18 +15,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    image = models.ImageField(null=True,blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
     @property
     def imageURL(self):
         try:
-            url =self.image.url
+            url = self.image.url
         except:
             url = ""
         return url
-
 
 
 class Order(models.Model):
@@ -38,6 +38,13 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        order_items = self.orderitem_set.all()
+        for
+        cart_total = 1
+        return cart_total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
@@ -45,8 +52,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return str(self.id)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
